7e18dfb89555d694c7b60e98220ca6432a605382
hiromi.ogawa <hiromi.ogawa.1223@gmail.com>
2018-11-30 13:45:52 +0000

    for heroku

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,59 @@
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :require_user_logged_in
+    
+    def index
+        @tasks = Task.order(created_at: :desc).page(params[:page]).per(3)
+    end
+    
+    def show
+    end
+    
+    def new
+        @task = Task.new
+    end
+    
+    def create
+        @task = current_user.tasks.build(task_params)
+        
+        if @task.save
+            flash[:success] = 'Taskが正常に投稿されました'
+            redirect_to @task
+        else
+            @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+            flash.now[:danger] = 'Taskが投稿されませんでした'
+            render :new
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        
+        if @task.update(task_params)
+            flash[:success] = 'Taskは正常に更新されました'
+            redirect_to @task
+        else
+            flash.now[:danger] = 'Task は更新されませんでした'
+            render  :edit
+        end    
+    end
+    
+    def destroy
+        @task.destroy
+
+        flash[:success] = 'Task は正常に削除されました'
+        redirect_to tasks_url
+    end
+    
+    private
+    
+    def set_task
+    @task = Task.find(params[:id])
+    end
+    
+   def task_params
+    params.require(:task).permit(:content, :status)
+   end
+end
